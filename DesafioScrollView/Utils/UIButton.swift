e8f10ef14ce2da1172ec8c4dd9aa8ac568236378
phmb2 <phmb002@gmail.com>
2021-04-25 00:10:35 -0300

    Project settings

---
+++ DesafioScrollView/Utils/UIButton.swift
@@ -0,0 +1,39 @@
+//
+//  UIButton.swift
+//  DesafioScrollView
+//
+//  Created by Pedro Barbosa on 03/04/21.
+//
+
+import UIKit
+
+@IBDesignable extension UIButton {
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+        set {
+            guard let uiColor = newValue else { return }
+            layer.borderColor = uiColor.cgColor
+        }
+    }
+}
